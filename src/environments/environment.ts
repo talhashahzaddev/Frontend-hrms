8727a494d3a5c105008ab7ff503f86fda3ebf7e9
talhashahzaddev <abu.sufian6098@gmail.com>
2025-10-22 21:23:32 +0500

    updates

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://localhost:60485/api',
+  apiKey: '',
+  appName: 'HRMS Frontend',
+  auth: {
+    tokenKey: 'token',
+    refreshTokenKey: 'refresh_token',
+    userKey: 'user'
+  }
+};
