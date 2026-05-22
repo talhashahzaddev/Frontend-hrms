dcdd2e421a5ff194f83b1e1d7ef795982ab187f8
talhashahzaddev <TalhaQuantum1@>
2026-05-22 21:15:04 +0500

    bugfix/setting-rolese

--- src/app/features/settings/components/company-policies/policy-list.component.ts
+++ src/app/features/settings/components/company-policies/policy-list.component.ts
@@ -161,7 +161,6 @@ export class PolicyListComponent implements OnInit, OnDestroy {
   }
 
   hasPermission(actionKey: string): boolean {
-    if (this.isSuperAdmin) return true;
     return this.authService.hasMenuPermission('Settings', 'Company Policies', actionKey);
   }
 }
